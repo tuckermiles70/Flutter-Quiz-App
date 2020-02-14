0f5fc477da8f212a63f36177036b9441213d6861
Tucker <tmiles7@vols.utk.edu>
2020-02-13 19:52:39 -0500

    added quiz and result widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -20,7 +20,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
-  final questions = const [
+  final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
       'answers': ['Black', 'Red', 'Green', 'White'],
@@ -40,7 +40,7 @@ class _MyAppState extends State<MyApp> {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
-    if (_questionIndex < questions.length) {
+    if (_questionIndex < _questions.length) {
       print('We have more questions!');
     } else {
       print('No more questions!');
@@ -54,23 +54,9 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: _questionIndex < questions.length
-            ? Column(
-                children: [
-                  Question(
-                    questions[_questionIndex]
-                        ['questionText'], //accessing value of key questionText
-                  ),
-                  ...(questions[_questionIndex]['answers'] as List<String>)
-                      .map((answer) {
-                    //function gets executed for every element in answers list. had to "cast" of sorts becuase Dart couldn't infer enough here
-                    return Answer(_answerQuestion, answer);
-                  }).toList() //list of widgets
-                ],
-              )
-            : Center(
-                child: Text('You did it!'),
-              ),
+        body: _questionIndex < _questions.length
+            ? Quiz(answerQuestion: _answerQuestion, questionIndex: _questionIndex, questions: _questions)
+            : Result(),
       ),
     );
   }
---
+++ lib/quiz.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final Function answerQuestion;
+
+  Quiz(
+      {@required this.questions,
+      @required this.answerQuestion,
+      @required this.questionIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(
+          questions[questionIndex]
+              ['questionText'], //accessing value of key questionText
+        ),
+        ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
+          //function gets executed for every element in answers list. had to "cast" of sorts becuase Dart couldn't infer enough here
+          return Answer(answerQuestion, answer);
+        }).toList() //list of widgets
+      ],
+    );
+  }
+}
