15f490d9d86922076ef74a4821a320abe9a6f131
Tucker <tmiles7@vols.utk.edu>
2020-02-13 19:38:40 -0500

    Added "You did it!" screen

--- lib/main.dart
+++ lib/main.dart
@@ -20,47 +20,57 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    }, //{} just some syntax for a map
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'What is your favorite food?',
+      'answers': ['Pizza', 'Sandwich', 'Salad', 'Steak'],
+    },
+  ];
+
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      }, //{} just some syntax for a map
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'What is your favorite food?',
-        'answers': ['Pizza', 'Sandwich', 'Salad', 'Steak'],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]
-                  ['questionText'], //accessing value of key questionText
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              //function gets executed for every element in answers list. had to "cast" of sorts becuase Dart couldn't infer enough here
-              return Answer(_answerQuestion, answer);
-            }).toList() //list of widgets
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]
+                        ['questionText'], //accessing value of key questionText
+                  ),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    //function gets executed for every element in answers list. had to "cast" of sorts becuase Dart couldn't infer enough here
+                    return Answer(_answerQuestion, answer);
+                  }).toList() //list of widgets
+                ],
+              )
+            : Center(
+                child: Text('You did it!'),
+              ),
       ),
     );
   }
